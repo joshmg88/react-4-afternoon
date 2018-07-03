89deb239bab05fcb62a37edd623068de6bbeb640
joshmg88 <joshmg88@gmail.com>
2018-07-03 16:19:05 -0500

    initial commit

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    };
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+      // console.log(this.props);
+      this.setState({ students: res.data });
+    })
+  }
+
+
   render() {
+    // console.log(this.state.students)
+    // console.log(this.props)
+    let studentName = this.state.students.map((e, i) => {
+      return (
+        <Link key={i} to={`/student/${e.id}`}>
+          <h3>{`${e.first_name} ${e.last_name}`}</h3>
+        </Link>)
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {studentName}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state = {
+      studentInfo: {}
+    };
 
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      // console.log(res.data)
+      this.setState({ studentInfo: res.data });
+    })
+  }
+
+
   render() {
+    let { studentInfo } = this.state
+    console.log(this.props);
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{studentInfo.first_name} {studentInfo.last_name}</h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
     )
   }
